3774e15bb9a2df75dff3660237cef517c9b444d0
DmytroKyryliuk2023 <dmitrokiriluk1208@gmail.com>
2026-02-24 21:08:34 +0200

    feat: dependencies and configs for testing

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: ['./src/test/setup.js'],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'html', 'lcov'],
+      exclude: [
+        'node_modules/',
+        'src/test/',
+        '**/*.test.js',
+        '**/*.test.jsx',
+      ],
+    },
+  },
+});
