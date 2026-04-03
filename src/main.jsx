7a2d327d2beadac76e1747301e9aee8a3f3b376a
DmytroKyryliuk2023 <dmitrokiriluk1208@gmail.com>
2026-04-03 21:21:31 +0300

    feat: add posthog-js for analytics tracking

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import posthog from 'posthog-js';
+
+posthog.init('phc_sYTJUY56AMQXL2wd5QSNPtKWFwBnNDXJAG2L2sUuiGSN', {
+api_host: 'https://eu.i.posthog.com',
+person_profiles: 'identified_only', // або 'always' для анонімних користувачів
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
